19b14165f4d6b9ee7d036dabeebcfdfb262a1a3f
Saadharta <fret-de@hotmail.fr>
2020-07-01 11:38:12 +0200

    update

--- tests/wm_get_IP_test/wm_get_IP_test.ino
+++ tests/wm_get_IP_test/wm_get_IP_test.ino
@@ -5,7 +5,8 @@ uint8_t macAddr[6];
 char strMacAddr[32];
 
 /*WiFiManager vars*/
-const char* ssid = "NEED_WM_CFG"; /* Does someone got a better name ?*/
+char ssid[20];
+//const char* ssid = "NEED_WM_CFG"; /* Does someone got a better name ?*/
 const char* pwd = "password"; /* need 8 char pwd else autoConnect fails.*/
 WiFiManager wm;
 
@@ -14,7 +15,9 @@ void get_MAC(){
   Serial.print("ESP BOARD MAC Address: ");
   WiFi.macAddress(macAddr);
   snprintf( strMacAddr, sizeof(strMacAddr), "%02X:%02X:%02X:%02X:%02X:%02X", macAddr[0],macAddr[1],macAddr[2],macAddr[3],macAddr[4],macAddr[5]);
+  snprintf(ssid, sizeof(ssid),"%s%02X:%02X","airquality_",macAddr[4], macAddr[5]);
   Serial.println(strMacAddr);
+  Serial.println(ssid);
   Serial.flush();
 }
 

a71e77958e44edd254053a1823e6e78a11ff3b01
Saadharta <fret-de@hotmail.fr>
2020-07-27 11:27:41 +0200

    SPIFFS boots up on laptop however fail to initialize if i compile and upload from my home machine.

--- src/neosensor/neosensor.ino
+++ src/neosensor/neosensor.ino
@@ -10,6 +10,7 @@ mqttsclient client;
 wifimanager wm;
 esp32_memory mem;
 StaticJsonDocument<(JSON_OBJECT_SIZE (128))> jso;
+
 //base_class bc[MAX_SENSORS];
 
 void func1(byte *p, unsigned int l){
@@ -21,14 +22,16 @@ void func2(byte *p, unsigned int l){
     log_debug("func 2 was called");
     return;
 }
+
+base_class bc1("airquality",func1);
+base_class bc2("temperature",func2);
+
 void setup(){   
     delay(3000);
     Serial.begin(115200);
     Serial.println(F("Setup begin"));
     mem.begin();
     wm.setup();
-    base_class bc1("airquality",func1);
-    base_class bc2("temperature",func2);
     byte b[] = {0x43, 0x55,0x87};
     bc1.on_message(b, 0);
     bc1.add();
@@ -37,9 +40,18 @@ void setup(){
     client.connect();
     client.serialize();
     log_debug("Attempting to read conf from mem");
-
-    jso = mem.read(MQTT_FILE);
-    serializeJsonPretty(jso,Serial);
+    if(mem.exists(CRED_FILE)){
+        jso = mem.read(CRED_FILE);
+        serializeJsonPretty(jso,Serial);
+    }else{
+        log_error("this file wasn't found in memory");
+    }
+    if(mem.exists(MQTT_FILE)){
+        jso = mem.read(MQTT_FILE);
+        serializeJsonPretty(jso,Serial);
+    }else{
+        log_error("this file wasn't found in memory");
+    }
     Serial.println(F("\nsetup end"));
 }
 

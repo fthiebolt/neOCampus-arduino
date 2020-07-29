b706d5bb477cb8fb719682a0a337adbe8c432b2c
Saadharta <fret-de@hotmail.fr>
2020-07-29 15:13:48 +0200

    implementing https class [setting up self WiFiClientSecure to use it in HTTPClient.begin]

--- src/neosensor/src/httpsclient.cpp
+++ src/neosensor/src/httpsclient.cpp
@@ -4,14 +4,20 @@ httpsclient::httpsclient () {};
 
 StaticJsonDocument<CRED_JSON_SIZE> httpsclient::get_credentials(uint8_t *mac_addr){
     log_debug("--- beg of httpsclient::get_credentials ---");
+    const char* root_ca = NULL;
+    const char* _cacert = NULL;
+    const char* _clicert = nullptr;
+    const char* _clikey = nullptr;
     WiFiClientSecure *wcsClient = new WiFiClientSecure;
-    wcsClient->setCACert(_root_ca);
+    wcsClient->setCACert(_cacert);
+    wcsClient->setCertificate(_clicert);
+    wcsClient->setPrivateKey(_clikey);
 
     //snprintf(_url, 90, "%s%s", AUTH_SERVER, CRED_REQ);
     //log_debug(_url)
     //snprintf(_url, 90, "%s%02X:%02X:%02X:%02X:%02X:%02X", _url, mac_addr[0],mac_addr[1],mac_addr[2],mac_addr[3],mac_addr[4],mac_addr[5]);
     //log_debug(_url);
-    if(!_https.begin(*wcsClient, "https://sensocampus.univ-tlse3.fr/device/credentials?mac=24:0A:C4:02:8B:60")){
+    if(!_https.begin(*wcsClient,"https://sensocampus.univ-tlse3.fr/device/credentials?mac=24:0A:C4:02:8B:60")){
         log_error("Beginning of https communication failed" );
     }else{
         log_debug("https communication established")
@@ -30,7 +36,6 @@ StaticJsonDocument<CRED_JSON_SIZE> httpsclient::get_credentials(uint8_t *mac_add
     Serial.println("Credentials requested from auth");
     log_debug(_url);
     _https.end();
-    delete wcsClient;
     // Test if parsing succeeds.
     if (error) {
         Serial.print(F("deserializeJson() failed: "));

0a7a79122701cb12d42a9b0f574ebaa1044410dc
francois <francois@clever.amilab.irit.fr>
2023-05-26 08:37:36 +0200

    update

--- arduinoIDE_esp32_boards/local_deploy.sh
+++ arduinoIDE_esp32_boards/local_deploy.sh
@@ -4,6 +4,7 @@
 #
 # Deploying specific settings for board
 #
+# F.Thiebolt    may.23  updated comments
 # F.Thiebolt    aug.22  update for 2.0.4 sdk with esp32 target only
 #                       i.e does not apply to esp32 derivatives like esp32c3
 # F.Thiebolt    aug.21  initial release
@@ -15,9 +16,11 @@
 [ $# -lt 1 ] && { echo -e "\n###ERROR missing base dir parameter !" >&2; exit 1; }
 [ -d $1 ] || { echo -e "\n###ERROR non existing directory '$1'!" >&2; exit 1; }
 
-echo -e "Copying additional board files ..."
+echo -e "\t[addon] Copying additional libraries/headers files ..."
 
-# copying files
+
+#
+# copying lwip + header(s)
 cp -af opt.h $1/esp32/include/lwip/lwip/src/include/lwip/
 [ $? -ne 0 ] && { echo -e "\n###ERROR copying some files to '$1/esp32/include/lwip/lwip/src/include/lwip/'" >&2; exit 1; }
 chmod a+r $1/esp32/include/lwip/lwip/src/include/lwip/opt.h
@@ -25,6 +28,10 @@ chmod a+r $1/esp32/include/lwip/lwip/src/include/lwip/opt.h
 cp -af *.a $1/esp32/lib/
 [ $? -ne 0 ] && { echo -e "\n###ERROR copying some files to '$1/esp32/lib/'" >&2; exit 1; }
 chmod a+r $1/esp32/lib/*.a
+
+echo -e "\t[addon] successfully copied lwip+headers :)"
+
+
 
 #set +x
 
